ecf8f6036529e3413976ec8eabadbfa8153bb9ae
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2021-09-03 20:12:11 +0300

    Cases for Exam Project

---
+++ src/main/java/pages/ParentPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class ParentPage {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/parentTest/ParentTest.java
@@ -0,0 +1,7 @@
+package parentTest;
+
+public class ParentTest {
+    public static void main(String[] args) {
+
+    }
+}
