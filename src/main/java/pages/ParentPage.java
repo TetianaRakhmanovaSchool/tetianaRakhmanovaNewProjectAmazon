0b4a941a14a1e64115f516ba1d6981b69a367da1
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2021-09-03 20:20:33 +0300

    locators file

--- src/main/java/pages/ParentPage.java
+++ src/main/java/pages/ParentPage.java
@@ -1,6 +1,11 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class ParentPage {
+    public ParentPage(WebDriver webDriver, String s) {
+    }
+
     public static void main(String[] args) {
 
     }
